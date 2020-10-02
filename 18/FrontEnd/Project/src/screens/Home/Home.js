fb52d54f6b85da345734578012d034397a9567d2
zenon191 <37409773+zenon191@users.noreply.github.com>
2020-10-02 08:21:05 +0700

    Home part2 (#3)
    * test
    
    * code home part 2
    
    Co-authored-by: namdongngac <37470354+namdongngac@users.noreply.github.com>

--- 18/FrontEnd/Project/src/screens/Home/Home.js
+++ 18/FrontEnd/Project/src/screens/Home/Home.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image, Dimensions, StatusBar } from 'react-native';
+import { View, StyleSheet, Text, Image, Dimensions, StatusBar, ScrollView } from 'react-native';
 import {
-	Header,
-	Left,
-	Title,
-	Body,
 	Container,
-	Button,
-	Icon,
-  Right,
-  Thumbnail
+	
+  Thumbnail,
+  Row, Col
 } from 'native-base';
 import header_home from '../../constants/images/header_home.png';
 import avatar from '../../constants/images/avatar.png';
+import ItemHome from '../../components/ItemHome';
 
 const Home = () => {
   return (
     <Container>
-      <StatusBar backgroundColor="#fff"/>
-
       <View style={styles.header}>
       <Image source={header_home} style={styles.header_home} />
       </View>
@@ -27,7 +21,39 @@ const Home = () => {
       </View>
 
       <View style={styles.container}>
-        <Text style={styles.text_header}>Hack nao 1500 ban 2020</Text>
+        <Text style={styles.text_header}>Hack não 1500 bản 2020</Text>
+
+        <ScrollView style={styles.content}>
+          <Text style={styles.text_title}>Học sách</Text>
+          <Row>
+            <Col style={styles.collums}>
+              <ItemHome icon={"headphones"} label={"Audio & Video"} />
+            </Col>
+            <Col style={styles.collums}>
+            <ItemHome icon={"file-alt"} label={"Kiểm tra"} />
+            </Col>
+            <Col style={styles.collums}>
+
+            <ItemHome icon={"book-open"} label={"Luyện tập"} />
+            </Col>
+            
+          </Row>
+
+          <Text style={styles.text_title}>Học mở rộng</Text>
+          <Row>
+            <Col style={styles.collums}>
+              <ItemHome icon={"music"} label={"Bài hát"} />
+            </Col>
+            <Col style={styles.collums}>
+            <ItemHome icon={"search"} label={"Từ điển tra câu"} />
+            </Col>
+            <Col style={styles.collums}>
+
+            <ItemHome icon={"puzzle-piece"} label={"Truyện chêm tham khảo"} />
+            </Col>
+            
+          </Row>
+        </ScrollView>
       </View>
       
     </Container>
@@ -44,7 +70,6 @@ const styles = StyleSheet.create({
     resizeMode: 'stretch'
   },
   header: {
-    paddingTop: 20,
     backgroundColor: '#F3F3F3',
     borderBottomColor: '#000000'
   },
@@ -55,16 +80,24 @@ const styles = StyleSheet.create({
     color: '#00CDDA',
     fontSize: 24,
     fontWeight: "700",
-    alignContent: 'center',
-    justifyContent: 'center',
     textAlign: 'center',
     marginTop: 80
   },
   avatar: {
     position: 'absolute',
-    top: screen.height * 0.18,
+    top: screen.height * 0.15,
     left: 170,
     zIndex: 10
+  },
+  collums: {
+    marginLeft: 15
+  },
+  text_title: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: '#000',
+    marginLeft: 15,
+    marginBottom: 10
   }
 })
 
