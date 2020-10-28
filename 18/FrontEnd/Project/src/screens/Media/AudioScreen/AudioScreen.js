d6d560cc65295d743f6d6c1be6add2923a86e304
phuocck <phuocnguyen1999010@gmail.com>
2020-10-28 22:58:44 +0700

    tich hop api cho man list audio

--- 18/FrontEnd/Project/src/screens/Media/AudioScreen/AudioScreen.js
+++ 18/FrontEnd/Project/src/screens/Media/AudioScreen/AudioScreen.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'native-base';
 import { StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
+// import { useQuery } from '../../../hooks/axios.hook';
 import ItemMedia from '../../../components/ItemMedia';
 
 const dataItemLeft = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29];
@@ -10,7 +12,21 @@ const dataItemRight = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30];
 
 const AudioScreen = () => {
   const navigation = useNavigation();
+//   const {data, loading} = useQuery({
+//     url: '/units',
+//   });
+// console.log(!loading && data)
 
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/units')
+      .then(res => setData(res.data))
+      .catch(er => console.log(er.response))
+      .finally(() => console.log('ket thuc'))
+  }, []);
+
+  console.log(data)
   return (
     <Row>
       <Col style={styles.collums}>
