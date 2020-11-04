c9162665ee7cb801958082a1d1e8b5e4f839481b
Nguyen Thanh Nam <ntnam7399@gmail.com>
2020-11-04 14:58:02 +0700

    fix axios

--- 18/FrontEnd/Project/src/screens/Media/AudioScreen/AudioScreen.js
+++ 18/FrontEnd/Project/src/screens/Media/AudioScreen/AudioScreen.js
@@ -7,9 +7,6 @@ import axios from 'axios';
 // import { useQuery } from '../../../hooks/axios.hook';
 import ItemMedia from '../../../components/ItemMedia';
 
-const dataItemLeft = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29];
-const dataItemRight = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30];
-
 const AudioScreen = () => {
   const navigation = useNavigation();
 //   const {data, loading} = useQuery({
@@ -20,23 +17,22 @@ const AudioScreen = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/units')
+    axios.get('https://hacknao-api.herokuapp.com/units')
       .then(res => setData(res.data))
       .catch(er => console.log(er.response))
       .finally(() => console.log('ket thuc'))
   }, []);
 
-  console.log(data)
   return (
     <Row>
       <Col style={styles.collums}>
-        {dataItemLeft.map((item, index) => (
-          <ItemMedia label={`Audio Unit ${item}`} key={index} onClick={() => navigation.navigate('Audio Unit')} />
+        {!!data && data.data.map((item, index) => {return (index%2==0) ? (item.name) : null}).filter(n => n).map((item, index) => (
+          <ItemMedia label={`Audio ${item}`} key={index} onClick={() => navigation.navigate('Audio Unit')} />
         ))}
       </Col>
       <Col style={styles.collums}>
-        {dataItemRight.map((item, index) => (
-          <ItemMedia label={`Audio Unit ${item}`} key={index} onClick={() => navigation.navigate('Audio Unit')} />
+        {!!data && data.data.map((item, index) => {return (index%2==1) ? (item.name) : null}).filter(n => n).map((item, index) => (
+          <ItemMedia label={`Audio ${item}`} key={index} onClick={() => navigation.navigate('Audio Unit')} />
         ))}
       </Col>
     </Row>
